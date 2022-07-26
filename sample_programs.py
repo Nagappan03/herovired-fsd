3bbd242961f0c7804e6dad4d2fb4eb3932b84181
Nagappan S <Karthiknano932@gmail.com>
2022-07-26 22:23:19 +0530

    Assignment 2 added

---
+++ sample_programs.py
@@ -0,0 +1,44 @@
+# num = int(input("Please enter a number: "))
+# if (num >= 10 and num <= 100 and not(num >= 50 and num <= 60)):
+#     print("true")
+# else:
+#     print("false")
+
+
+# if (num < 10 or num > 100):
+#     print("true")
+# else:
+#     print("false")
+
+
+# if (num > 10 and num < 100 and not(num == 50)):
+#     print("true")
+# else:
+#     print("false")
+
+
+# if ((num >= 10 and num <= 30) and (num >= 40 and num <= 50) and (num >= 60 and num <= 100)):
+#     print("true")
+# else:
+#     print("false")
+
+
+# opt = [33, 100, 77, 42, 12]
+# output = []
+# for i in opt:
+#     if i % 2 != 0:
+#         output.append(i)
+# print(output)
+
+
+# sum = 0
+# mul = 1
+# while True:
+#     num = int(input("Please enter a number: "))
+#     if(num != -1):
+#         sum = sum + (num * mul)
+#         mul = mul + 1
+#     else:
+#         sum = sum + (num * mul)
+#         break
+# print(sum)
